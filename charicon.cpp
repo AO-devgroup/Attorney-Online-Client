ac461d0c43933b604669f903d196dccadd25f9a7
David Skoland <davidskoland@gmail.com>
2016-07-17 23:30:06 +0200

    started on character select. did not quite succeed

---
+++ charicon.cpp
@@ -0,0 +1,6 @@
+#include "charicon.h"
+
+charicon::charicon()
+{
+
+}
---
+++ charicon.h
@@ -0,0 +1,12 @@
+#ifndef CHARICON_H
+#define CHARICON_H
+
+#include <QWidget>
+
+class charicon : public QPushButton
+{
+public:
+  charicon();
+};
+
+#endif // CHARICON_H
