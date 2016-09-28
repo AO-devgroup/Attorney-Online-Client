4cd5a117169d1b43d68112c9e22fc35349fb6609
David Skoland <davidskoland@gmail.com>
2016-09-28 14:21:04 +0200

    changed port

--- network_handler.h
+++ network_handler.h
@@ -27,7 +27,7 @@ public:
 
   //QString ms_hostname = "dev.aceattorneyonline.com";
   QString ms_hostname = "master.aceattorneyonline.com";
-  int ms_port = 27015;
+  int ms_port = 27016;
 
   QVector<server_type> m_server_list;
 
